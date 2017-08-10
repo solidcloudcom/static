d6e1f8fc6df34cf7560ad48bba0d2a7ca60944ea
NikitaRudy <rudiynikita@gmail.com>
2017-08-10 10:44:01 +0300

    typescript migration

---
+++ src/server/app.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as passport from 'passport';
+import * as session from 'express-session';
+import * as mongoose from 'mongoose';
+// import config from './config';
+// import router from './router/router.js';
